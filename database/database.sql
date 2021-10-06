0d06c123a83d0d4615721216eb97eaeea8ad1b72
Goncalo <2201131@my.ipleiria.pt>
2021-10-06 21:48:38 +0100

    update database
    update database

--- database/database.sql
+++ database/database.sql
@@ -13,15 +13,40 @@ CREATE TABLE IF NOT EXISTS clients(
 	CONSTRAINT uk_clients_Nif UNIQUE (nif)
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS contributors(
+		id INT UNSIGNED AUTO_INCREMENT,
+		contributorname VARCHAR(100) NOT NULL,
+        nif VARCHAR(9) NOT NULL,
+		birthdate DATE NOT NULL,
+        mail VARCHAR(100) NOT NULL,
+        phonenumber VARCHAR(40) NOT NULL,
+        registrationdate DATE NOT NULL,
+        specialty VARCHAR(100) NOT NULL,
+	CONSTRAINT contributors_id PRIMARY KEY(id),
+	CONSTRAINT uk_contributors_Nif UNIQUE (nif)
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS cars(
 		id INT UNSIGNED AUTO_INCREMENT,
         vin VARCHAR(100) NOT NULL,
         registration VARCHAR(100) NOT NULL,
         purschasedate DATE NOT NULL,
         kilometers INT NOT NULL,
-		state VARCHAR(100),
+		state VARCHAR(100) NOT NULL,
         clientId INT UNSIGNED NOT NULL,
 	CONSTRAINT cars_id PRIMARY KEY(id),
     CONSTRAINT uk_cars_vin UNIQUE (vin),
 	CONSTRAINT fk_cars_clientId FOREIGN KEY(clientId) REFERENCES clients(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS repairs(
+		id INT UNSIGNED AUTO_INCREMENT,
+		kilometers INT NOT NULL,
+        repairdate DATE NOT NULL,
+        repairdescription VARCHAR(100) NOT NULL,
+        carId INT UNSIGNED NOT NULL,
+        contributorId INT UNSIGNED NOT NULL,
+	CONSTRAINT repairs_id PRIMARY KEY(id),
+	CONSTRAINT fk_repairs_carId FOREIGN KEY(carId) REFERENCES cars(id),
+    CONSTRAINT fk_repairs_contributorId FOREIGN KEY(contributorId) REFERENCES contributors(id)
 ) ENGINE=InnoDB;
