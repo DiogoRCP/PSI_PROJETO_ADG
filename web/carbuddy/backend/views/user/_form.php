1770ae500daefc967e20217e487392b9c8012d42
Goncalo <2201131@my.ipleiria.pt>
2021-10-19 13:04:41 +0100

    gestão de users funcional

---
+++ web/carbuddy/backend/views/user/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Users */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="users-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'userpassword')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'usertype')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nif')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'birsthday')->textInput() ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phonenumber')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'registrationdate')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ web/carbuddy/backend/views/user/view.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Users */
+
+$this->title = $model->username.' ('.$model->nif.')';
+
+
+\yii\web\YiiAsset::register($this);
+?>
+<div class="users-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+        <?= Html::a('Back', ['index'], ['class' => 'btn btn-primary']) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'username',
+            'usertype',
+            'nif',
+            'birsthday',
+            'email:email',
+            'phonenumber',
+            'registrationdate',
+        ],
+    ]) ?>
+
+</div>
