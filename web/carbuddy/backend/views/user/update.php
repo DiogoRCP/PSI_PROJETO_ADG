813cd3d340a7519726421e6a6e59a1bdf342ccf0
Goncalo <2201131@my.ipleiria.pt>
2021-10-19 13:08:20 +0100

    botões back

--- web/carbuddy/backend/views/user/update.php
+++ web/carbuddy/backend/views/user/update.php
@@ -5,14 +5,14 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Users */
 
-$this->title = 'Update Users: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Update User: ' .$model->username.' ('.$model->nif.')';
 ?>
 <div class="users-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    <p>
+        <?= Html::a('Back', ['index'], ['class' => 'btn btn-primary']) ?>
+    </p>
 
     <?= $this->render('_form', [
         'model' => $model,
