1eceb1d3c876c0c933bed6c217ba4b469322170d
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-10-26 12:38:27 +0100

    inicio da implementação da api

--- web/carbuddy/frontend/assets/AppAsset.php
+++ web/carbuddy/frontend/assets/AppAsset.php
@@ -16,6 +16,7 @@ class AppAsset extends AssetBundle
         'css/style.css',
     ];
     public $js = [
+        'js/Cars.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ web/carbuddy/frontend/web/js/Cars.js
@@ -0,0 +1,17 @@
+SearchCar('wBA1V910805G93301', 2016);
+
+function SearchCar(vin, year) {
+    $.ajax({
+        url: `https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${vin}`,
+        type: "GET",
+        data: {'modelyear': year, 'format':'json'},
+        dataType: "json"
+    }).done(function (resposta) {
+        console.log(resposta);
+    }).fail(function (jqXHR, textStatus) {
+        console.log("Request failed: " + textStatus);
+
+    }).always(function () {
+        console.log("completou");
+    });
+}
