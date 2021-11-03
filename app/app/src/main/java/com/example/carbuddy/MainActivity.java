8d516cedeb39c792c1195b69ffcf6991ee8d74c4
Goncalo <2201131@my.ipleiria.pt>
2021-11-03 15:13:46 +0000

    Já obtem dados de API's, da nossa API n obtem porque esta em localhost

--- app/app/src/main/java/com/example/carbuddy/MainActivity.java
+++ app/app/src/main/java/com/example/carbuddy/MainActivity.java
@@ -12,8 +12,17 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.regex.Pattern;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -79,8 +88,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-    public void onClickLogin(View view) {
+    public void onClickLogin(View view ) {
         if(efetuarLogin()==true) {
+
+
+
+
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url ="http://localhost:8080/users";
+
+// Request a string response from the provided URL.
+            //JsonObjectRequest
+            //JsonArrayRequest
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            Toast.makeText(MainActivity.this, response.substring(0,500), Toast.LENGTH_LONG);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, "error", Toast.LENGTH_LONG);
+                }
+            });
+
+// Add the request to the RequestQueue.
+            queue.add(stringRequest);
+
+
+
+
+
+
+
             Intent paginalinicial = new Intent(this, Pagina_Inicial.class);
             startActivity(paginalinicial);
         }
