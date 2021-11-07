b3c3e34471a9119576ca13167c6b768510a77797
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-11-07 15:37:04 +0000

    api to web

--- web/carbuddy/frontend/config/main.php
+++ web/carbuddy/frontend/config/main.php
@@ -11,6 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'modules' => [
+        'api' => [
+            'class' => 'frontend\modules\api\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
@@ -36,14 +41,70 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule',
+                    'controller' => 'v1/user',
+                    'pluralize' => false,
+                    'extraPatterns' => [
+                        'GET total' => 'total',
+                        'GET set/{limit}' => 'set',
+                        'POST post' => 'post',
+                        'PUT put/{id}' => 'put',
+                        'DELETE  delete/{id}' => 'delete'
+                    ],
+                    'tokens' => ['{id}' => '<id:\d+>', '{limit}' => '<limit:\d+>',],
+
+                    'controller' => 'v1/repairs',
+                    'pluralize' => false,
+                    'extraPatterns' => [
+                        'GET total' => 'total',
+                        'GET set/{limit}' => 'set',
+                        'POST post' => 'post',
+                        'PUT put/{id}' => 'put',
+                        'DELETE  delete/{id}' => 'delete'
+                    ],
+                    'tokens' => ['{id}' => '<id:\d+>', '{limit}' => '<limit:\d+>',],
+
+                    'controller' => 'v1/companies',
+                    'pluralize' => false,
+                    'extraPatterns' => [
+                        'GET total' => 'total',
+                        'GET set/{limit}' => 'set',
+                        'POST post' => 'post',
+                        'PUT put/{id}' => 'put',
+                        'DELETE  delete/{id}' => 'delete'
+                    ],
+                    'tokens' => ['{id}' => '<id:\d+>', '{limit}' => '<limit:\d+>',],
+
+                    'controller' => 'v1/cars',
+                    'pluralize' => false,
+                    'extraPatterns' => [
+                        'GET total' => 'total',
+                        'GET set/{limit}' => 'set',
+                        'POST post' => 'post',
+                        'PUT put/{id}' => 'put',
+                        'DELETE  delete/{id}' => 'delete'
+                    ],
+                    'tokens' => ['{id}' => '<id:\d+>', '{limit}' => '<limit:\d+>',],
+
+                    'controller' => 'v1/contributors',
+                    'pluralize' => false,
+                    'extraPatterns' => [
+                        'GET total' => 'total',
+                        'GET set/{limit}' => 'set',
+                        'POST post' => 'post',
+                        'PUT put/{id}' => 'put',
+                        'DELETE  delete/{id}' => 'delete'
+                    ],
+                    'tokens' => ['{id}' => '<id:\d+>', '{limit}' => '<limit:\d+>',],
+                ]
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
---
+++ web/carbuddy/frontend/modules/api/controllers/ContributorsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace frontend\modules\api\controllers;
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+use yii\filters\auth\HttpBasicAuth;
+use frontend\models\User;
+
+class ContributorsController extends ActiveController
+{
+    public $modelClass = 'app\models\Contributors';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => QueryParamAuth::className()
+        ];
+
+        return $behaviors;
+    }
+    public function auth($token) {
+
+        $user = User::findIdentityByAccessToken($token);
+        if ($user !=null)
+        {
+            return $user;
+        } return null;
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    public function actionTotal(){
+        $Contributorssmodel = new $this -> modelClass;
+        $recs = $Contributorssmodel::find() -> all();
+        return ['total' => count($recs)];
+    }
+
+    //http://localhost:8080/v1/contributors/set/3
+
+    public function actionSet($limit){
+        $Contributorssmodel = new $this -> modelClass;
+        $rec = $Contributorssmodel::find() -> limit($limit) -> all();
+        return ['limite' => $limit, 'Records' => $rec ];
+    }
+
+// http://localhost:8080/v1/contributors/post
+
+    public function actionPost() {
+
+        $name=\Yii::$app -> request -> post('name');
+
+        $Contributorssmodel = new $this -> modelClass;
+        $Contributorssmodel -> name = $name;
+
+        $ret = $Contributorssmodel -> save(false);
+        return ['SaveError' => $ret];
+    }
+
+    //http://localhost:8080/v1/contributors/delete/id
+
+    public function actionDelete($id)
+    {
+        $Contributorssmodel = new $this->modelClass;
+        $ret=$Contributorssmodel->deleteAll("id=".$id);
+        if($ret)
+            return ['DelError' => $ret];
+        throw new \yii\web\NotFoundHttpException("Client id not found!");
+    }
+}
