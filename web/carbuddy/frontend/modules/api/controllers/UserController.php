9e03bc57b516686fb01c96ceb2dac39d1dbef37a
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-11-08 18:44:07 +0000

    user put e inicio do rbac

--- web/carbuddy/frontend/modules/api/controllers/UserController.php
+++ web/carbuddy/frontend/modules/api/controllers/UserController.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace frontend\modules\api\controllers;
+
 use common\models\User;
+use PHPUnit\Util\Log\JSON;
+use yii\helpers\VarDumper;
 use yii\rest\ActiveController;
 use yii\filters\auth\QueryParamAuth;
+
 class UserController extends ActiveController
 {
     public $modelClass = 'common\models\User';
@@ -17,42 +21,48 @@ class UserController extends ActiveController
 
         return $behaviors;
     }
-    public function auth($token) {
+
+    public function auth($token)
+    {
 
         $user = User::findIdentityByAccessToken($token);
-        if ($user !=null)
-        {
+        if ($user != null) {
             return $user;
-        } return null;
+        }
+        return null;
     }
 
     public function actionIndex()
     {
         return $this->render('index');
     }
-    public function actionTotal(){
-        $Usersmodel = new $this -> modelClass;
-        $recs = $Usersmodel::find() -> all();
+
+    public function actionTotal()
+    {
+        $Usersmodel = new $this->modelClass;
+        $recs = $Usersmodel::find()->all();
         return ['total' => count($recs)];
     }
 
     //http://localhost:8080/api/user/set/3
 
-    public function actionSet($limit){
-        $Usersmodel = new $this -> modelClass;
-        $rec = $Usersmodel::find() -> limit($limit) -> all();
-        return ['limite' => $limit, 'Records' => $rec ];
+    public function actionSet($limit)
+    {
+        $Usersmodel = new $this->modelClass;
+        $rec = $Usersmodel::find()->limit($limit)->all();
+        return ['limite' => $limit, 'Records' => $rec];
     }
 
 // http://localhost:8080/api/user/post
 
-    public function actionPost() {
+    public function actionPost()
+    {
 
-        $username=\Yii::$app -> request -> post('username');
-        $Usersmodel = new $this -> modelClass;
-        $Usersmodel -> username = $username;
+        $username = \Yii::$app->request->post('username');
+        $Usersmodel = new $this->modelClass;
+        $Usersmodel->username = $username;
 
-        $ret = $Usersmodel -> save(false);
+        $ret = $Usersmodel->save(false);
         return ['SaveError' => $ret];
     }
 
@@ -61,33 +71,32 @@ class UserController extends ActiveController
     public function actionDelete($id)
     {
         $Usersmodel = new $this->modelClass;
-        $ret=$Usersmodel->deleteAll("id=".$id);
-        if($ret)
+        $ret = $Usersmodel->deleteAll("id=" . $id);
+        if ($ret)
             return ['DelError' => $ret];
         throw new \yii\web\NotFoundHttpException("Client id not found!");
     }
 
-    public function actionPut($id){
+    public function actionPut($id)
+    {
+        if (\Yii::$app->user->getId() == $id) {
+            $user = json_decode(\Yii::$app->request->rawBody);
 
-        $username=\Yii::$app -> request -> post('username');
-        $usertype=\Yii::$app -> request -> post('usertype');
-        $nif=\Yii::$app -> request -> post('nif');
-        $birsthday=\Yii::$app -> request -> post('birsthday');
-        $email=\Yii::$app -> request -> post('email');
-        $phonenumber=\Yii::$app -> request -> post('phonenumber');
+            $Usermodel = new $this->modelClass;
+            $rec = $Usermodel::find()->where('id = ' . $id)->one();
 
-        $Usermodel = new $this->modelClass;
-        $rec = $Usermodel::find()->where('id = '.$id)->one();
+            if (isset($user->username)) $rec->username = $user->username;
+            if (isset($user->usertype)) $rec->usertype = $user->usertype;
+            if (isset($user->nif)) $rec->nif = $user->nif;
+            if (isset($user->birsthday)) $rec->birsthday = $user->birsthday;
+            if (isset($user->email)) $rec->email = $user->email;
+            if (isset($user->phonenumber)) $rec->phonenumber = $user->phonenumber;
 
-        $rec->username = $username;
-        $rec->usertype = $usertype;
-        $rec->nif = $nif;
-        $rec->birsthday = $birsthday;
-        $rec->email = $email;
-        $rec->phonenumber = $phonenumber;
-
-        $rec->save(false);
-        return ['SaveError1' => $rec];
-        //throw new \yii\web\NotFoundHttpException("Client id not found!");
+            $rec->save(false);
+            //return ['SaveError1' => $rec];
+            //throw new \yii\web\NotFoundHttpException("Client id not found!");
+        } else {
+            return "Sem permissões";
+        }
     }
 }
