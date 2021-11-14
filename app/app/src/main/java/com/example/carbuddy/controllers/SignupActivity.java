7da45c2ac81127f00b909198ba947b4f4dfa425d
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-11-14 01:27:14 +0000

    signup date picker

--- app/app/src/main/java/com/example/carbuddy/controllers/SignupActivity.java
+++ app/app/src/main/java/com/example/carbuddy/controllers/SignupActivity.java
@@ -3,6 +3,7 @@ package com.example.carbuddy.controllers;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,8 @@ import com.example.carbuddy.models.Signup;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText username, email, nif, birsthday, phonenumber, password;
+    private EditText username, email, nif, phonenumber, password;
+    private DatePicker birsthday;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +27,7 @@ public class SignupActivity extends AppCompatActivity {
         username = findViewById(R.id.txusername);
         email = findViewById(R.id.txemail);
         nif = findViewById(R.id.txnif);
-        birsthday = findViewById(R.id.txbirsthday);
+        birsthday = (DatePicker)findViewById(R.id.txbirsthday);
         phonenumber = findViewById(R.id.txphone);
         password = findViewById(R.id.txpassword);
     }
@@ -35,7 +37,7 @@ public class SignupActivity extends AppCompatActivity {
         form.setUsername(username.getText().toString());
         form.setEmail(email.getText().toString());
         form.setNif(nif.getText().toString());
-        form.setBirsthday(birsthday.getText().toString());
+        form.setBirsthday(birsthday.getYear()+"/"+birsthday.getMonth()+"/"+birsthday.getDayOfMonth());
         form.setPhonenumber(phonenumber.getText().toString());
         form.setPassword(password.getText().toString());
 
