a59ed7c1dda949122ebdf71c2d21f3bc5a84236c
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-11-21 03:08:50 +0000

    car index com botões em vez de tabela

--- web/carbuddy/frontend/views/site/index.php
+++ web/carbuddy/frontend/views/site/index.php
@@ -10,36 +10,4 @@ $this->title = 'CarBuddy';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <img src="../images/logo_white.png" width="250">
-    </div>
-
-    <?php if (Yii::$app->user->can('frontendCrudVehicle')) { ?>
-        <div class="index-content">
-            <?= GridView::widget([
-                'dataProvider' => $dataProvider,
-                'filterModel' => $searchModel,
-                'columns' => [
-                    //'id',
-                    'registration',
-                    'vin',
-                    'brand',
-                    'model',
-                    //'color',
-                    //'carType',
-                    //'displacement',
-                    //'fuelType',
-                    'modelyear',
-                    //'kilometers',
-                    //'state',
-                    //'userId',
-
-                    // ['class' => 'yii\grid\ActionColumn'],
-                ],
-            ]); ?>
-        </div>
-    <?php } else {
-        echo $this->render('about');
-        $this->title = 'CarBuddy';
-    } ?>
 </div>
