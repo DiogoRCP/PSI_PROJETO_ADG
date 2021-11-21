2df2d32ca8e399017267643aa6fb94b54453a199
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-11-21 10:37:04 +0000

    pequena correção do commit anterior

--- web/carbuddy/frontend/views/car/index.php
+++ web/carbuddy/frontend/views/car/index.php
@@ -7,13 +7,14 @@ use yii\helpers\VarDumper;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Cars */
+/* @var $create */
 $this->title = 'Vehicles';
 ?>
 <div class="cars-index">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= ($create == true)?Html::a('Create Vehicle', ['create'], ['class' => 'btn btn-secondary']):"" ?>
+        <?= ($create)?Html::a('Create Vehicle', ['create'], ['class' => 'btn btn-secondary']):"" ?>
     </p>
 
     <div class="body-content">
