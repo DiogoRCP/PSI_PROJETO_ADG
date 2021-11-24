aa420dcb1ea366d4f6f0ddc081f1377c73bf9529
Andreia Agostinho Batista <batista.a.andreia@gmail.com>
2021-11-24 10:41:14 +0000

    Estilização do CRUD de Users

--- web/carbuddy/frontend/views/User/update.php
+++ web/carbuddy/frontend/views/User/update.php
@@ -12,10 +12,14 @@ $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="users-update">
 
+    <div class="users-content">
+
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
+    </div>
 
 </div>
+
