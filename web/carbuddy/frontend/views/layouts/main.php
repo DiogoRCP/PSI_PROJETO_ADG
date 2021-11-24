a34b164ee1f99d30556140cbdaa32378bceee76c
Andreia Agostinho Batista <batista.a.andreia@gmail.com>
2021-11-24 13:10:31 +0000

    Atualização do main layout

--- web/carbuddy/frontend/views/layouts/main.php
+++ web/carbuddy/frontend/views/layouts/main.php
@@ -37,8 +37,8 @@ AppAsset::register($this);
         ]);
         $menuItems = [
             //['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            //['label' => 'About', 'url' => ['/site/about']],
+            //['label' => 'Contact', 'url' => ['/site/contact']],
         ];
         if (Yii::$app->user->isGuest) {
             $menuItems2 = [
