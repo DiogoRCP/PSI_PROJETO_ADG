4eca27729d05cc5cbda9a9dece20ba3a68b9204e
Goncalo <2201131@my.ipleiria.pt>
2021-11-30 15:56:27 +0000

    Att

--- web/carbuddy/backend/tests/unit/CompaniesTest.php
+++ web/carbuddy/backend/tests/unit/CompaniesTest.php
@@ -3,6 +3,7 @@ namespace backend\tests;
 
 use backend\models\Companies;
 use backend\controllers\CompanyController;
+use Yii;
 use yii\caching\DummyCache;
 
 
@@ -11,6 +12,7 @@ class CompaniesTest extends \Codeception\Test\Unit
     /**
      * @var \backend\tests\UnitTester
      */
+
     protected $tester;
     
     protected function _before()
@@ -22,17 +24,30 @@ class CompaniesTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testSomeFeature()
+    public function testCreateModel()
     {
-        //Testes ao modelo Companies
-        $id = 1;
         $model = new Companies();
+    }
+    public function testTableName()
+    {
+        $model = new Companies();
+        $table_name = $model->tableName();
+    }
+    public function testFind()
+    {
+        $id = 1;
+        $model = Companies::find($id);
 
+    }
+    public function testCreateModelWithData()
+    {
+        Companies::getDb();
+        $model = new Companies();
         $model->companyname = "Midas";
-        $model->phonenumber = "912123065";
-        $model->nif = "123412341";
-        $model->email = "Midas@hotmail.com";
-
+        $model->email = "Midas@midas.pt";
+        $model->phonenumber = "912123049";
+        $model->nif = "123123053";
+        $model->save();
         expect('model should blow', $model->save());
     }
 }
