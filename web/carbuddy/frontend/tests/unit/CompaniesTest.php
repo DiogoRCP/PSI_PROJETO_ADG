ba17624c513954338d7c586c226052ae64697f7c
Goncalo <2201131@my.ipleiria.pt>
2021-12-03 12:50:24 +0000

    testes falsos

--- web/carbuddy/frontend/tests/unit/CompaniesTest.php
+++ web/carbuddy/frontend/tests/unit/CompaniesTest.php
@@ -38,17 +38,32 @@ class CompaniesTest extends \Codeception\Test\Unit
     {
         $model = new Companies();
 
+        $model->setname('NomeEmpresaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+        $this->assertFalse($model->validate(['companyname']));
+
         $model->setname('NomeEmpresa');
         $this->assertTrue($model->validate(['companyname']));
+
+        $model->setemail('nomeempresa@n1111111111111111111111111111111111111111111111111111111111111111111111111111111111ome.pt');
+        $this->assertFalse($model->validate(['email']));
 
         $model->setemail('nomeempresa@nome.pt');
         $this->assertTrue($model->validate(['email']));
 
+        $model->setnif('9129120113');
+        $this->assertFalse($model->validate(['nif']));
+
         $model->setnif('912912013');
         $this->assertTrue($model->validate(['nif']));
 
+        $model->setphonenumber('9129120114444444444444444444444444444444444444444444444444444444443');
+        $this->assertFalse($model->validate(['phonenumber']));
+
         $model->setphonenumber('912912013');
         $this->assertTrue($model->validate(['phonenumber']));
+
+        $model->setreistrationdate('2021-11-30 17:19:075555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555');
+        $this->assertTrue($model->validate(['registrationdate']));
 
         $model->setreistrationdate('2021-11-30 17:19:07');
         $this->assertTrue($model->validate(['registrationdate']));
