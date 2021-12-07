19185ab2998e7ba67b1c72769423ba0505103888
Goncalo <2201131@my.ipleiria.pt>
2021-12-07 17:06:20 +0000

    teste concluido

--- web/carbuddy/frontend/tests/functional/ContactCest.php
+++ web/carbuddy/frontend/tests/functional/ContactCest.php
@@ -8,8 +8,29 @@ class ContactCest
     }
 
     // tests
-    public function tryToTest(FunctionalTester $I)
+    public function contactWithoutLogin(FunctionalTester $I)
     {
+        $I->amOnPage(['site/contact']);
+        $I->submitForm('#contact-form', []);
+        $I->expectTo('see validations errors');
+        $I->see('Contact', 'h1');
+        $I->see('Name cannot be blank');
+        $I->see('Email cannot be blank');
+        $I->see('Subject cannot be blank');
+        $I->see('Body cannot be blank');
+        $I->fillField('Name', 'erau');
+        $I->fillField('E-mail', 'erau@mail.com');
+        $I->fillField('Subject', 'subject');
+        $I->fillField('Body', 'Message');
+        $I->fillField('Verify Code', '#ADAF$$#');
+    }
+
+    public function contactWithLogin(FunctionalTester $I)
+    {
+        $I->amOnPage('/site/login');
+        $I->fillField('Username', 'erau');
+        $I->fillField('Password', 'password_0');
+        $I->click('login-button');
         $I->amOnPage(['site/contact']);
         $I->submitForm('#contact-form', []);
         $I->expectTo('see validations errors');
