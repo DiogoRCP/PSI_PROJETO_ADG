c7f728c61c3c311dcc9513e55c4bc816b65f1ee5
Goncalo <2201131@my.ipleiria.pt>
2021-12-07 16:03:31 +0000

    WEB_teste contact

---
+++ web/carbuddy/frontend/tests/functional/ContactCest.php
@@ -0,0 +1,22 @@
+<?php
+namespace frontend\tests\functional;
+use frontend\tests\FunctionalTester;
+class ContactCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->amOnPage(['site/contact']);
+        $I->submitForm('#contact-form', []);
+        $I->expectTo('see validations errors');
+        $I->see('Contact', 'h1');
+        $I->see('Name cannot be blank');
+        $I->see('Email cannot be blank');
+        $I->see('Subject cannot be blank');
+        $I->see('Body cannot be blank');
+    }
+}
