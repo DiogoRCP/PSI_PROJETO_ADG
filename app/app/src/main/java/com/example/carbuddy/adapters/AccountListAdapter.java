2044aa899292a4f3da27c37299ccdaef210dc0e7
Andreia Agostinho Batista <batista.a.andreia@gmail.com>
2021-12-09 16:42:35 +0000

    APP - Account List Adapter

---
+++ app/app/src/main/java/com/example/carbuddy/adapters/AccountListAdapter.java
@@ -0,0 +1,14 @@
+package com.example.carbuddy.adapters;
+
+import android.accounts.Account;
+import android.content.Context;
+
+import com.example.carbuddy.adapters.RepairListAdapter;
+
+import java.util.ArrayList;
+
+public class AccountListAdapter extends RepairListAdapter {
+    public AccountListAdapter(Context context, ArrayList<Account> lstAccount) {
+        super();
+    }
+}
