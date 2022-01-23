b791f6d7f6eeb067ba852f3ba7472f6eccaaf0a0
Goncalo <2201131@my.ipleiria.pt>
2022-01-23 20:11:38 +0000

    APP-singleton car correção e comments

--- app/app/src/main/java/com/example/carbuddy/singletons/CarSingleton.java
+++ app/app/src/main/java/com/example/carbuddy/singletons/CarSingleton.java
@@ -127,80 +127,117 @@ public class CarSingleton {
             RequestQueue queue = Volley.newRequestQueue(context);
             // URL do endpoint da API
             String url = IP + "repairs/history/" + car.getId() + ACCESSTOKEN(context);
-
+            // cria um request JsonArrayRequest GET
             JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
                     (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
                         @Override
                         public void onResponse(JSONArray response) {
+                            //criação de uma lista de repairs para guardar as repairs recebidas
                             ArrayList<Repair> repairs = new ArrayList<>();
-
+                            //Ciclo for para receber todas as repairs e 1 a 1 adicioná-la à lista repairs
                             for (int i = 0; i < response.length(); i++) {
                                 try {
+                                    //Receber um objeto repair na posição i
                                     JSONObject resp = response.getJSONObject(i);
+                                    //Converter o json em objeto
                                     Repair repair = (Repair) objectjsonConvert(resp, Repair.class);
+                                    //Adicionar a repair na posição i à lista depois de convertida para objeto
                                     repairs.add(repair);
                                 } catch (JSONException e) {
+                                    //Exception
                                     e.printStackTrace();
                                 }
                             }
+                            //Atribuir a lista de reparações ao carro recebido como argumento na função
                             car.setRepairs(repairs);
-
+                            //Chamar o listener
                             repairsListener.onRefreshRepair(repairs);
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
+                            //Mensagem de erro
                             Toast.makeText(context, R.string.NoConnection, Toast.LENGTH_SHORT).show();
                         }
                     });
-
+            // Adicionar pedido à fila
             queue.add(jsonArrayRequest);
         }
     }
 
+    /** Função que faz DELETE de um carro **/
     public void DeleteCar(Context context, int carId) {
+        // Se não houver conexão à internet mostra mensagem de erro
         if (!isInternetConnection(context)) {
             Toast.makeText(context, R.string.NoInternet, Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        // Se houver internet executa o código de delete car
+        else {
+            //criação de um request
             RequestQueue queue = Volley.newRequestQueue(context);
+            // URL do endpoint da API
             String url = IP + "cars/deleted/" + carId + ACCESSTOKEN(context);
 
+            // cria um request JsonArrayRequest DELETE
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                     (Request.Method.DELETE, url, null, new Response.Listener<JSONObject>() {
+                        //Executado com sucesso
                         @Override
                         public void onResponse(JSONObject response) {
                         }
                     }, new Response.ErrorListener() {
+                        //Possiveis erros ao apagar um carrp
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            try {
-                                switch (error.networkResponse.statusCode) {
-                                    case 200:
-                                        carsListener.onDeleteCreateCar();
-                                        break;
-                                    case 403:
-                                        Toast.makeText(context, R.string.NotDeleteCar, Toast.LENGTH_SHORT).show();
-                                        break;
-                                    default:
-                                        Toast.makeText(context, R.string.NoConnection, Toast.LENGTH_SHORT).show();
+                            String message = "";
+                            //Verificar que o volleyerror não é nulo
+                            if(error.networkResponse != null) {
+                                try {
+                                    switch (error.networkResponse.statusCode) {
+                                        case 200:
+                                            //Chamar o listener porque o pedido foi executado
+                                            carsListener.onDeleteCreateCar();
+                                            break;
+                                        case 403:
+                                            //carregar a variável message com Erro ao tentar apagar um carro com repairs
+                                            message = context.getResources().getString(R.string.NotDeleteCar);
+                                            break;
+                                        default:
+                                            //carregar a variável message com Erro de falha na conexão
+                                            message = context.getResources().getString(R.string.NoConnection);
+                                    }
+                                } catch (Exception ex) {
+                                    //Chamar o listener
+                                    carsListener.onDeleteCreateCar();
                                 }
-                            } catch (Exception ex) {
-                                carsListener.onDeleteCreateCar();
                             }
+                            else{
+                                //carregar a variável message com Erro de falha na conexão
+                                message = context.getResources().getString(R.string.NoConnection);
+                            }
+                            //Mostrar a mensagem de erro dentro da variavel message
+                            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
                         }
                     });
-
+            // Adicionar pedido à fila
             queue.add(jsonObjectRequest);
         }
     }
 
+    /** Função que faz POST de um carro **/
     public void AddCar(Context context, Car car) throws JSONException {
+        // Se não houver conexão à internet mostra mensagem de erro
         if (!isInternetConnection(context)) {
             Toast.makeText(context, R.string.NoInternet, Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        // Se houver internet executa o código de POST car
+        else {
+            //criação de um request
             RequestQueue queue = Volley.newRequestQueue(context);
+            // URL do endpoint da API
             String url = IP + "cars/post" + ACCESSTOKEN(context);
 
+            //Criação de um json object que recebe os dados do carro para realizar o seu post
             JSONObject carData = new JSONObject();
             carData.put("vin", car.getVin());
             carData.put("brand", car.getBrand());
@@ -214,30 +251,40 @@ public class CarSingleton {
             carData.put("displacement", car.getDisplacement());
             carData.put("state", car.getState());
 
+            // cria um request JsonArrayRequest POST
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                     (Request.Method.POST, url, carData, new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
+                            //Chamar o listener caso o pedido tenha sido executado
                             carsListener.onDeleteCreateCar();
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
+                            //Mensagem de erro
                             Toast.makeText(context, R.string.NoConnection, Toast.LENGTH_SHORT).show();
                         }
                     });
-
+            // Adicionar pedido à fila
             queue.add(jsonObjectRequest);
         }
     }
 
+    /** Função que faz PUT de um carro **/
     public void EditCar(Context context, Car car) throws JSONException {
+        // Se não houver conexão à internet mostra mensagem de erro
         if (!isInternetConnection(context)) {
             Toast.makeText(context, R.string.NoInternet, Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        // Se houver internet executa o código de PUT car
+        else {
+            //criação de um request
             RequestQueue queue = Volley.newRequestQueue(context);
+            // URL do endpoint da API
             String url = IP + "cars/put/" + car.getId() + ACCESSTOKEN(context);
 
+            //Criação de um json object que recebe os dados do carro para realizar o seu PUT
             JSONObject carData = new JSONObject();
             carData.put("vin", car.getVin());
             carData.put("brand", car.getBrand());
@@ -251,43 +298,52 @@ public class CarSingleton {
             carData.put("displacement", car.getDisplacement());
             carData.put("state", car.getState());
 
+            // cria um request JsonArrayRequest PUT
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                     (Request.Method.PUT, url, carData, new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
+                            //Chamar o listener caso o pedido tenha sido executado
                             carsListener.onDeleteCreateCar();
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
+                            //Mensagem de erro
                             Toast.makeText(context, R.string.NoConnection, Toast.LENGTH_SHORT).show();
                         }
                     });
-
+            // Adicionar pedido à fila
             queue.add(jsonObjectRequest);
         }
     }
 
+    //Obter a lista de todos os carros
     public ArrayList<Car> getCars() {
         return cars;
     }
 
+    //Listener dos carros
     public void setCarsListener(fragment_garage fragment) {
         this.carsListener = fragment;
     }
 
+    //Obter reparações de um dados carro
     public ArrayList<Repair> getRepairs(Car car) {
         return car.getRepairs();
     }
 
+    //Listener das repairs
     public void setRepairsListener(RepairFragment fragment) {
         this.repairsListener = fragment;
     }
 
+    //listener do delete car
     public void setDeleteListener(fragment_carInfo fragment) {
         this.carsListener = fragment;
     }
 
+    //listener do add car
     public void setCreateListener(fragment_form_car fragment) {
         this.carsListener = fragment;
     }
